d5a6fbb03617726ad2a21f9c8e8025612731d458
Alan Wani Vuciri <alan132301@gmail.com>
2026-03-24 23:13:44 +0300

    Completed assignment 1

--- ex2_fuel.c
+++ ex2_fuel.c
@@ -1,11 +1,26 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: Alan Wani Vuciri */
+/* Student Number: 25/U/BIO/024/GV*/
+
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+	
+float distanceTravelled;
+printf("Enter distance(km):%.2f\n", distanceTravelled);
+scanf("%f", &distanceTravelled);// reading the distance travelled from the user and storing in the variable distanceTravelled
+
+float fuelUsed;
+printf("Enter fuel used(liters):%.2f\n", fuelUsed);
+scanf("%f", &fuelUsed);
+
+float fuelEfficiency;
+fuelEfficiency = distanceTravelled/fuelUsed;// calculating fuel efficiency 
+
+printf("Fuel efficiency: %.2f km/L\n", fuelEfficiency);// printing result
+
+
 
     return 0;
 }
--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,24 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: Alan Wani Vuciri */
+/* Student Number: 25/U/BIO/024/GV*/
+
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+    float unitsConsumed, unitPrice;
+    
+    printf("Enter units consumed:%.2f\n", unitsConsumed);
+    scanf("%f", &unitsConsumed);// reading the units consumed from the user and storing it in the variable unitsConsumed
+    
+    printf("Enter cost per unit:%.2f\n", unitPrice);
+    scanf("%f", &unitPrice);// reading the cost per unit from the user and storing it in the variable unitPrice
+    
+    float totalBill = unitsConsumed*unitPrice;// calculating total cost
+    
+    printf("Total Bill:%.2f UGX\n", totalBill);
+    
+    
 
     return 0;
 }
